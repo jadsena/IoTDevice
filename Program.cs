3f0986a7d82f882da9f7d3c3fb367f82ed478cdf
Jean Alonso Duarte Sena <jadsena@hotmail.com>
2019-02-17 23:08:41 -0300

    Inicial

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace IoTDevice
+{
+    class Program
+    {
+        private static DeviceClient s_deviceClient;
+
+        // The device connection string to authenticate the device with your IoT hub.
+        // Using the Azure CLI:
+        // az iot hub device-identity show-connection-string --hub-name {YourIoTHubName} --device-id MyDotnetDevice --output table
+        private readonly static string s_connectionString = "HostName=RM330673FiapIotHub.azure-devices.net;DeviceId=Dispositivo1;SharedAccessKey=/AcDCh7EoOE7xHXU7zcYymiOw2cXHsltZB+lXlYhqA8=";
+
+        // Async method to send simulated telemetry
+        private static async void SendDeviceToCloudMessagesAsync()
+        {
+            // Initial telemetry values
+            double minTemperature = 20;
+            double minHumidity = 60;
+            Random rand = new Random();
+
+            
+            double currentTemperature = minTemperature + rand.NextDouble() * 15;
+            double currentHumidity = minHumidity + rand.NextDouble() * 20;
+
+            // Create JSON message
+            var telemetryDataPoint = new
+            {
+                temperature = currentTemperature,
+                humidity = currentHumidity
+            };
+            var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+            var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+            // Add a custom application property to the message.
+            // An IoT hub can filter on these properties without access to the message body.
+            message.Properties.Add("temperatureAlert", (currentTemperature > 30) ? "true" : "false");
+
+            // Send the telemetry message
+            await s_deviceClient.SendEventAsync(message);
+            Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("IoT Hub Quickstarts #1 - Simulated device. Ctrl-C to exit.\n");
+
+            // Connect to the IoT hub using the MQTT protocol
+            s_deviceClient = DeviceClient.CreateFromConnectionString(s_connectionString, TransportType.Mqtt);
+            System.Threading.Timer timer = new System.Threading.Timer(EventTimer, null, 0, 60000);
+            
+            Console.ReadLine();
+            if(timer != null) timer.Dispose();
+            if(s_deviceClient != null) s_deviceClient.Dispose();
+        }
+        private static void EventTimer(Object stateInfo)
+        {
+            SendDeviceToCloudMessagesAsync();
+        }
+    }
+}
